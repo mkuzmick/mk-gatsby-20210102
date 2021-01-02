60eeed6fe0cd2a766e7320eeb5de15e125bc0dc8
Marlon Kuzmick <mkuzmick@icloud.com>
2021-01-02 18:56:42 -0500

    Updates:

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,9 @@ export default function Footer() {
     <header id="header">
       <h1 style={{marginBottom: "0px"}}>{config.heading}</h1>
       <p style={{fontSize: "1em"}}>
-        in studying and practicing rhetoric, we attend to moves, moves that move you, turns of phrase (tropes) you return to again and again, but also tropes you turn to to "make it new." In whatever it is that comes after writing, what are the moves? Let us know, and we'll add them to our choreography . . . 
+        in studying and practicing rhetoric, we attend to <strong>moves</strong>, <strong>moves</strong> that <strong>move</strong> you, turns of phrase (tropes) you return to again and again, but also tropes you turn to to "make it new." Like juxtaposition, for instance.</p> 
+      <p style={{fontSize: "1em"}}>
+          In whatever it is that comes after writing, what are the <strong>moves?</strong> Let us know, and we'll add them to our choreography . . . 
       </p>
     </header>
   );
