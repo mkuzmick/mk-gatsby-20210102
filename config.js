4e53c2898f95a539c10396fc08ce05240c9fb199
Marlon Kuzmick <mkuzmick@icloud.com>
2021-01-02 19:07:34 -0500

    Updates: new form dynamic test

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Eventually', // <title>
-  manifestName: 'Eventually',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'MK Gatsby 20210102', // <title>
+  manifestName: 'MK Gatsby',
+  manifestShortName: 'MK', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   // social
   heading: 'moves',
   subHeading:
